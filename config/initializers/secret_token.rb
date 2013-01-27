001cf55e88c26e0081fe83a218621baa2d84d936
Gerrit Wanderer <wanderwelten+account@googlemail.com>
2013-01-27 11:01:36 +0100

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberCrud::Application.config.secret_token = '6d588a8c08df2990918b203f869789945e3db9c81dc0b4c8a4ff4f0aa588c46c47b2a78dad138b3aeaa1e536f88bc141858e60a1b6aabfdeec0d0011c27f2a16'
